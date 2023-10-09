ba929c50a6db95e05d6cdf89f082673e3c4092c7
aprashantz <prashantghi8@gmail.com>
2023-10-09 23:21:45 +0545

    add: TextConstant

---
+++ GitPing/Constant/TextConstant.swift
@@ -0,0 +1,5 @@
+class TextConstant {
+    static let kWelcome = "Welcome"
+    static let kSlogan = "Stay updated about what the devs are upto"
+    static let kGitHubSignInTitle = "Sign in with GitHub"
+}
